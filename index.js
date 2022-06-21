176d7365edac90f46a81a087d6d5eb0692085f08
cater20 <onyangoketta@gmail.com>
2022-06-21 22:42:39 +0300

    done with assignment

--- index.js
+++ index.js
@@ -9,15 +9,130 @@
  for you to use if you need it!
  */
 
+ const createEmployeeRecord =(array)=>{
+    return {
+    firstName:array[0],
+    familyName:array[1],
+    title:array[2],
+    payPerHour:array[3],
+    timeInEvents:[],
+    timeOutEvents:[],
+    }
+    return details
+}
+const createEmployeeRecords=(employee)=>{
+    return employee.map((array)=>{return createEmployeeRecord(array)})
+}
+//const createTimeInEvent = function(dateStamp){
+  //  let [date, hour] = dateStamp.split(' ')
+
+    //this.timeInEvents.push({
+      //  type: "TimeIn",
+        //hour: parseInt(hour, 10),
+        //date,
+    //})
+
+    //return this
+//}
+//const createTimeOutEvent = function(dateStamp){
+  //  let [date, hour] = dateStamp.split(' ')
+
+    //this.timeOutEvents.push({
+        //type: "TimeOut",
+      //  hour: parseInt(hour, 10),
+      //  date,
+   // })
+
+    //return this
+//}
+//const createTimeInEvent = (employee, dateStamp) => {
+  //let timeInEvents=[{type:TimeIn,
+  //  hours:dateTime,
+  //  date:dateTime,}];
+
+ //  employee.timeInEvents.push({
+  //  type:"TimeIn"
+//   })
+//}
+//console.log(employee); 
+
+//const hoursWorkedOnDate = function(neededDate){
+  //  let morning = this.timeInEvents.call(function(e){
+   //     return e.date === neededDate
+  //  })
+
+   // let evening = this.timeOutEvents.find(function(e){
+     //   return e.date === neededDate
+   // })
+
+    //return (evening.hour - morning.hour) / 100
+//}
+
+
+//const wagesEarnedOnDate = (neededDate) => {
+    //let rawWage = hoursWorkedOnDate.call(employee, neededDate)
+    ///    * this.payPerHour
+  //  return parseFloat(rawWage.toString())
+//}
+
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(neededDate){
+    let morning = this.timeInEvents.find(function(e){
+        return e.date === neededDate
+    })
+
+    let evening = this.timeOutEvents.find(function(e){
+        return e.date === neededDate
+    })
+
+    return (evening.hour - morning.hour) / 100
+}
+let wagesEarnedOnDate = function(neededDate){
+    let rawWage = hoursWorkedOnDate.call(this, neededDate)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+let findEmployeeByFirstName = function(array, firstName) {
+    return array.find(function(data){
+      return data.firstName === firstName
+    })
+  }
+  let calculatePayroll = function(arrayOfEmployees){
+    return arrayOfEmployees.reduce(function(memo, data){
+        return memo + allWagesFor.call(data)
+    }, 0)
+}
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
+ 
+     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
-}
-
+ 
+ return payable
+ }
